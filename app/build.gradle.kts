8c792971dbf20fbebcdd6ae2c21348fc79a65d76
Moonsu Kang <rkdms4971@naver.com>
2024-05-28 00:58:56 +0900

    study: 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,22 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+
+    // Compose dependencies
+    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.compose.ui:ui:1.4.4")
+    implementation("androidx.compose.material:material:1.4.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.6.0")
+
+    // Optional: For UI debugging
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.4")
 }
